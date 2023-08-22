c471fca925787c5d05be18c49301e03d69a8b0a3
marianafdo <142455911+marianafdo@users.noreply.github.com>
2023-08-22 10:17:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+/* 
+* Mariana Fernandes
+* Primeiro comentario
+*/
+console.log("Olá Mundo");
+//prompt("Digite seu nome");
+
+//Variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nome = "Mariana";
+const idade = 32;
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos");
+
+let primeiroNome = "Mariana";
+let sobrenome = "Fernandes";
+let minhaIdade = 32;
+let souEstudante = true;
+console.log("Meu nome é", primeiroNome, sobrenome, "e eu tenho", minhaIdade, "anos", souEstudante);
